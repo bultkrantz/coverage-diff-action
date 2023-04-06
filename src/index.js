16551b6faa214d49ba3b457bdda72598c6766a89
bultkrantz <marcus.hultkrantz@quinyx.com>
2023-04-06 09:38:19 +0200

    log pr title

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const { computeDiff } = require("./diff");
 const { addComment, deleteExistingComments } = require("./comment");
 
 const { context } = github;
-console.log("context", context.pull_request.title);
+console.log("context", context.payload.pull_request.title);
 async function run() {
   const tmpPath = await mkdir(path.join(process.env.GITHUB_WORKSPACE, "tmp"), {
     recursive: true,
